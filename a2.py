f069098d358235f31f9ed1866811225bfe3802ee
Justin DeGuzman <justindeguzman@dhcp-10-8-015-144.mobile.reshsg.uci.edu>
2026-02-09 18:54:08 -0800

    educed functionality of a2.py and moved to initiate ui.py

--- a2.py
+++ a2.py
@@ -4,35 +4,10 @@
 # justicd1@uci.edu
 # 72329664
 
-from shlex import split
-from command_processor import CommandProcessor
+import ui
 
 def main() -> None:
-    processor = CommandProcessor()
-
-    while True:
-        try:
-            line = input().strip()
-        except EOFError:
-            break
-
-        if line == "Q":
-            break
-        
-        try:
-            parts = split(line)
-        except ValueError:
-            print("ERROR")
-            continue
-
-        if len(parts) < 2:
-            print("ERROR")
-            continue
-
-        command, path, *options = parts
-        command = command.upper()
-        
-        processor.handle(command, path, options)
+    ui.run()
 
 if __name__ == "__main__":
     main()
--- ui.py
+++ ui.py
@@ -3,3 +3,33 @@
 # Justin DeGuzman
 # justicd1@uci.edu
 # 72329664
+
+from shlex import split
+from command_processor import CommandProcessor
+
+def run() -> None:
+    processor = CommandProcessor()
+
+    while True:
+        try:
+            line = input().strip()
+        except EOFError:
+            break
+
+        if line == "Q":
+            break
+        
+        try:
+            parts = split(line)
+        except ValueError:
+            print("ERROR")
+            continue
+
+        if len(parts) < 2:
+            print("ERROR")
+            continue
+
+        command, path, *options = parts
+        command = command.upper()
+        
+        processor.handle(command, path, options)
